5e32503eedabf63939c16029362806753aa19c97
Alejandro M. Moreno <alejandromorenomuller@gmail.com>
2024-04-28 22:25:42 -0500

    write first pass of logic to add sixteen container rows

--- script.js
+++ script.js
@@ -1,13 +1,38 @@
 function createRow(){
     // get container div
     const containerDiv = document.querySelector("#container");
-    const childDiv = document.createElement("div");
+    let childDiv;
+    let containerRow;
 
-    for (i=0; i < 16; i++){
-        containerDiv.appendChild(childDiv);
+    for (i=0; i < 14; i++){
+        containerRow = document.createElement("div");
+        containerRow.style.display = "flex";
+        for (i=0; i < 14; i++){
+            if (i === 0){
+                childDiv = document.createElement("div");
+                childDiv.style.backgroundColor = "red";
+                childDiv.style.width = "100px";
+                childDiv.style.height = "100px";
+                containerRow.appendChild(childDiv);
 
-
+            }
+        
+            childDiv = document.createElement("div");
+            childDiv.style.backgroundColor = "red";
+            childDiv.style.width = "100px";
+            childDiv.style.height = "100px";
+            containerRow.appendChild(childDiv);
+    
+    
+        }
+        containerDiv.appendChild(containerRow);
     }
+
+    // const childDivTwo = document.createElement("div");
+    // childDivTwo.style.backgroundColor = "red";
+    // childDivTwo.style.width = "100px";
+    // childDivTwo.style.height = "100px";
+    // containerDiv.appendChild(childDivTwo);
 
 }
 
